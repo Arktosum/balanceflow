2d8102e9ceecabe3bd38950da4dfafa377ba081e
Arktosum <siddhujaykay2@gmail.com>
2026-02-22 02:30:34 +0530

    fix : update pool params for ipv4 supabase connection on production environment

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -6,6 +6,9 @@ dotenv.config()
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 })
 
 db.connect((err, client, release) => {
